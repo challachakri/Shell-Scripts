d5bf133e0811838f99616a3924ebe06cf8ff6653
madhav411 <44801760+madhav411@users.noreply.github.com>
2019-08-13 10:17:00 +0530

    Update clargs.sh

--- Shell_Scripts/Shell Script Concepts/clargs.sh
+++ Shell_Scripts/Shell Script Concepts/clargs.sh
@@ -4,7 +4,7 @@ echo The 1st argument that i am passing through cmd is:$1
 echo The 2nd argument that i am passing through cmd is:$2
 echo $3
 echo $4
-echo Gives the number of arguments that we are passing:$#
+echo Gives the exact number of arguments that we are passing while executing a particular shell script:$#
 echo All the arguments as a one string will be represented by:$*
 echo Each argument as a one string will be represented by:$@
 echo The process-id of this particular script is:$$
